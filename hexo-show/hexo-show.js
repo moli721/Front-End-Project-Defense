d2b0a4c53032afa291ccfc6e6e84a316c52f9477
moli <2720316926@qq.com>
2024-09-27 21:57:23 +0800

    基本实现英雄界面

---
+++ hexo-show/hexo-show.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const panelWrappers = document.querySelectorAll('.panel-wrapper')
+
+    panelWrappers.forEach(wrapper => {
+        wrapper.addEventListener('click', () => {
+            removeActiveClasses()
+            wrapper.classList.add('active')
+            
+            // 获取当前点击的面板中的视频元素
+            const video = wrapper.querySelector('video')
+            if (video) {
+                // 重置视频播放时间到开始
+                video.currentTime = 0
+                // 重新播放视频
+                video.play()
+            }
+        })
+    })
+
+    function removeActiveClasses() {
+        panelWrappers.forEach(wrapper => {
+            wrapper.classList.remove('active')
+        })
+    }
+})
